a325290df0d8a9034c1b1501cd18a6f298d891e9
Mikael Lönn <mikael@sendify.se>
2021-11-03 18:13:05 +0100

    added address

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ const Main = styled.main`
   height: 100vh;
   place-items: center;
   align-content: center;
+  text-align: center;
 `;
 
 const Home = () => {
@@ -22,6 +23,10 @@ const Home = () => {
       <Main>
         <h1>Mikael och Fredrika</h1>
         <h2>2022-07-09</h2>
+        <div>
+          <h3>Wij Trädgårdar</h3>
+          <p>Wijberget 2, 816 30 Ockelbo</p>
+        </div>
       </Main>
     </div>
   );
