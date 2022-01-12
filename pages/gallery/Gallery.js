5eb5bd3a3129dee9e03ace276b97b5654c713c6e
Mikael Lönn <mikael@sendify.se>
2022-01-12 16:50:15 +0100

    Add wip gallery

--- pages/gallery/Gallery.js
+++ pages/gallery/Gallery.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import ImageGallery from 'react-image-gallery';
 
+const images = [
+  {
+    original: 'https://picsum.photos/id/1018/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+  },
+];
 const Gallery = () => {
-  return <></>;
+  return (
+    <div>
+      <ImageGallery items={images} />
+    </div>
+  );
 };
 
 export default Gallery;
