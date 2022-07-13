62044509a345ecbdb96a848c01fb84f7543fb3a5
Mikael Lönn <mikael@sendify.se>
2022-07-13 15:03:34 +0200

    add image gallery

--- pages/_app.js
+++ pages/_app.js
@@ -6,12 +6,18 @@ import Page from '../components/Page';
 import { GlobalStyle } from '../styles/globalStyles';
 import theme from '../styles/theme';
 import Head from 'next/head';
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <link rel="stylesheet" href="https://use.typekit.net/iaz7vbu.css" />
-        <script src="//widget.cloudinary.com/global/all.js" type="text/javascript"/>
+        <script
+          src="//widget.cloudinary.com/global/all.js"
+          type="text/javascript"
+          async
+          defer
+        />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
