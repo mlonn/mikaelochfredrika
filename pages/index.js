0c64c0bdb13b9bc16ebf2b8ca6ccbeeca2479040
Mikael Lönn <mikael@sendify.se>
2022-07-14 15:34:24 +0200

    change text on home page

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,36 @@
 import React from 'react';
 import Button from '../components/Button';
 import Link from 'next/link';
+
 const Home = () => {
   return (
     <>
       <div>
         <h3>HEM</h3>
         <h4>Aktuell Information</h4>
-        <h5>Det är officiellt dags!</h5>
+        <h5>Nu är det officiellt! </h5>
       </div>
       <p>
-        <b>Mikael och Fredrika är “tying-the-knot” som man säger!</b>
+        <b>Man och Fru!</b>
         <br />
         <br />
-        Vi hoppas självklart att ni kan vara med och förgylla vår dag med er
-        närvaro, vi vill ha alla våra nära och kära på plats för en rolig fest
-        tillsammans allihopa, i Ockelbo nära Fredrikas hemstad Sandviken.
+        Mikael och Fredrika är nu gifta med varandra och hade en helsike bra
+        fest med de bästa bröllopsgästerna!
         <br />
         <br />
-        Och när en mjukvaruingenjör och en designingenjör gifter sig måste de ju
-        självklart ha en hemsida som Fredrika designat och Mikael sedan skapat!
-        Här kommer vi att ta in anmälningar till bröllopet så vi kan skala
-        utefter alla som vill komma, samt ha som bildgalleri att minnas med för
-        alltid.
+        Så kul att ni kunde vara med och förgylla våran dag i Ockelbo när vi sa
+        ja och ble äkta makar. Nu kommer hemsidan agera som bildgalleri, och ett
+        vackert minne från denna dagen som vi aldrig vill glömma - så ge oss
+        alla bilder ni har! Ladda upp dem under fliken bildgalleri - tokiga som
+        fina, vi vill ha allt!
+        <br />
+        <br />
+        <Link passHref href={'/gallery'}>
+          <Button>Bildgalleri</Button>
+        </Link>
+        <br />
+        <br />
+        Tack igen för en bra fest - vi är mer än nöjda!
       </p>
     </>
   );
